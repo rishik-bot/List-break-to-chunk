6800d77bb394478c9e31ebbedd75a174e0a55b05
Rishik <68709235+rishik-bot@users.noreply.github.com>
2020-08-21 01:46:21 +0530

    Add files via upload

---
+++ List break to chunks.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import ast
+input_str = input()
+input_list = ast.literal_eval(input_str)
+lis=input_list[0]
+k=input_list[1] #chunks
+
+length=len(lis)
+mod=length%k 
+pdiv=length//k
+x=0 #number of lists after creating chunks
+if mod==0:
+    x=pdiv
+else:
+    x=pdiv+1
+row=[]
+list1=[list(row) for i in range(x)]
+
+while len(lis)>=k:
+    for i in range(pdiv):
+        for j in range(k):
+            list1[i].append(lis[0])
+            lis.pop(0)
+
+while lis:
+    for i in range(mod):
+        list1[-1].append(lis[0])
+        lis.pop(0)
+
+
+for i in list1:
+    print(i)
+
+
+# In[ ]:
+
+
+
+
